a53a934503e95a9a4933bb5906109f583bf3e82f
Sainandan_P_V <115761820+SainandanPV@users.noreply.github.com>
2025-07-08 00:09:59 +0530

    Update DBConnection.java

--- Assignment_6_UserRegistration/User_Registration/src/com/litmus7/user/util/DBConnection.java
+++ Assignment_6_UserRegistration/User_Registration/src/com/litmus7/user/util/DBConnection.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DBConnection {
 	private static final String URL="jdbc:mysql://localhost:3306/usersdb";
 	private static final String USER = "root";
-    private static final String PASSWORD = "sainandan";
+    	private static final String PASSWORD = "sainandan";
     
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
+    	public static Connection getConnection() throws SQLException {
+        	return DriverManager.getConnection(URL, USER, PASSWORD);
+    	}
 }
